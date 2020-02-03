5c39141e4464b3ff0acbed1c8565263c0d64f8b5
Ba1r <60482377+Ba1r@users.noreply.github.com>
2020-02-02 22:29:04 -0800

    Update Problem 1.cpp

--- Problem 1.cpp
+++ Problem 1.cpp
@@ -2,30 +2,9 @@
 #include <algorithm>
 #include <vector>
 #include <string>
+#include <numeric>
 
 using namespace std;
-
-string lower(string s) {
-	for (int i = 0; i < s.size(); i++) {
-		s[i] = tolower(s[i]);
-	}
-	return s;
-}
-
-pair<int, int> convert(string s) {
-	pair<int, int> p;
-	int sum = 0;
-	for (int i = 0; i < s.size(); i++) {
-		if (s[i] >= '0' && s[i] <= '9') {
-			sum += (s[i] - '0');
-		}
-		else {
-			sum += (s[i] - 'a');
-		}
-	}
-	p.first = sum;
-	return p;
-}
 
 int main()
 {
@@ -51,14 +30,21 @@ int main()
 
 	cout << count_if(a.begin(), a.end(), [](string& i) {return count_if(i.begin(), i.end(), isdigit); }) << "\n";
 
-	transform(a.begin(), a.end(), a.begin(), lower);
-	
+	transform(a.begin(), a.end(), a.begin(), [](string& i) {transform(i.begin(), i.end(), i.begin(), tolower); return NULL; });
+
 	vector<pair<int, int>> pa(a.size());
 	int i = 0;
-	transform(a.begin(), a.end(), pa.begin(), [&i](string& p) {pair<int, int> s = convert(p); s.second = i; i++; return s; });
-	
-	sort(pa.begin(), pa.end(), [&](pair<int, int> one, pair<int, int> two) {return one.first < two.first; });
-	
-	
+	transform(a.begin(), a.end(), pa.begin(), [&i](string& p) {pair<int, int> s; s.first = accumulate(p.begin(), p.end(), 0);  s.second = i; i++; return s; });
 
+	sort(pa.begin(), pa.end(), [&](pair<int, int> one, pair<int, int> two) {return one.first < two.first; });
+	const auto val = make_pair(75, 0);
+
+	auto c = lower_bound(pa.begin(), pa.end(), val, [](const pair<int, int>& p1, const pair<int, int>& p2) {return p1.first < p2.first; });
+	if (pa.end() == c) {
+		cout << "NONE";
+	}
+	else {
+		cout << a[c->second];
+	}
 }
+

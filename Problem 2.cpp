ce1327565a821fbd86a74244aa3e40c9a131081c
Ba1r <60482377+Ba1r@users.noreply.github.com>
2020-02-07 13:00:15 -0800

    Update Problem 2.cpp

--- Problem 2.cpp
+++ Problem 2.cpp
@@ -5,26 +5,23 @@ using namespace std;
 
 int main()
 {
-    int x1, x2, x3, x4;
-    cin >> x1;
-    cin >> x2;
-    cin >> x3;
-    cin >> x4;
-    
+    vector<int> x;
+
+    for (int i = 0; i < 4; i++) {
+        int c;
+        cin >> c;
+        x.push_back(c);
+    }
+
     int minx, maxx;
 
-    cout << (minx = min(min(x1, x2), min(x3, x4)));
-    cout << (maxx = max(max(x1, x2), max(x3, x4)));
+    cout << (minx = *min_element(x.begin(), x.end())) << "\n";
+    cout << (maxx = *max_element(x.begin(), x.end())) << "\n";
 
     int x5;
     cin >> x5;
 
-    if (x5 > maxx) {
-        x5 = maxx;
-    }
-    else if(x5 < minx) {
-        x5 = minx;
-    }
-
+    x5 = max(min(x5, maxx), minx);
+    cout << x5;
 }
 
